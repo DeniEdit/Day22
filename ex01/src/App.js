ae11e6b20ad513a049ce4e7fa7b7dd2518b8399f
DeniEdit <milicevic.deni@gmail.com>
2021-03-29 16:51:47 +0200

    Exercise 01

--- ex01/src/App.js
+++ ex01/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <hi>Hello, React!</hi>
+                <h1>Hello, React!</h1>
             </div>
         )
     }
