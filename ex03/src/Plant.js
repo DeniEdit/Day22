803cac11b9848a3bc0516dc858b5b67e3d535759
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-03-31 10:40:17 +0200

    Exercise 03

--- ex03/src/Plant.js
+++ ex03/src/Plant.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 
 class Plant extends Component {
     render() {
-        return (
-            <h2>This is a Plant Component!</h2>
-        )
-    }
+        return <h2>This is a Plant Component!</h2>
+}
 }
 
 export default Plant;
